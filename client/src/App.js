60a9b0cf5bf2c5b9c0673f776b5a556692e05db8
Erika <eyardumian@gmail.com>
2017-11-26 18:22:16 -0800

    all data displays

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ class App extends Component {
     super(props);
     this.state = {
       schoolName: '',
+      debt: '',
+      tuition: '',
       // schoolData: {}
     }
   }
@@ -20,11 +22,12 @@ class App extends Component {
      fetch(requestUrl)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.results[0]['school.name'])
+      // console.log(data.results[0]['school.name'])
+      console.log(data.results[0])
       this.setState({
         schoolName: data.results[0]['school.name'],
-        schoolData: data.results
-      // schoolData: school
+        debt: data.results[0]['2015.aid.median_debt.completers.overall'],
+        tuition: data.results[0]['2015.cost.tuition.in_state']
     })
     console.log(this.state.schoolName);
   });
@@ -33,14 +36,18 @@ class App extends Component {
   setSchool(event) {
     event.preventDefault();
     this.setState({
-      schoolName: event.target.value
+      schoolName: event.target.value,
+      debt: event.target.value,
+      tuition: event.target.value
     });
   };
 
-  cancelCourse = () => {
-  document.getElementById("create-course-form").reset();
-}
-
+  getInitialState() {
+      return {typed: ''};
+    };
+  onBlur(event) {
+    this.setState({typed: event.target.value});
+  };
 
   render() {
     // let message;
@@ -52,13 +59,16 @@ class App extends Component {
     // const schoolname = this.state.schoolName[0];
     // const {schooName} = this.state;
     return (
+
       <div>
-        <form id="create-course-form" action="/school" method="GET" id="myform">
-          <input type="text" className="form-control" id="enter_text" onChange={this.setSchool.bind(this)} />
-            <button onClick={this.fetchSchool.bind(this)} type="submit" className="btn btn-primary" id="text-enter-button button submit">Submit</button>
+        <form action="/school" method="GET" id="create-course-form">
+          <input type="text" className="form-control" id="enter_text" onBlur={this.setSchool.bind(this)} />
+            <button  onClick={this.fetchSchool.bind(this)} type="submit" className="btn btn-primary" id="text-enter-button button submit">Submit</button>
         </form>
         <div>
         <p>School: { this.state.schoolName } </p>
+        <p>Median Debt: { this.state.debt } </p>
+        <p>Tuition: { this.state.tuition } </p>
         </div>
       </div>
     );
